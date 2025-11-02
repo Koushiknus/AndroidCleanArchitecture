4a7a8eea419a4187974fbe1b752ca4a7bd88f22d
Koushik <koushiknus@gmail.com>
2025-11-01 19:32:55 -0500

    Update : Implemented Nav Graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 // Hilt
     implementation ("com.google.dagger:hilt-android:2.52")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     kapt ("com.google.dagger:hilt-compiler:2.52")
 // Testing
     testImplementation ("junit:junit:4.13.2")
--- app/src/main/java/com/android/cleanarchitecture/presentation/nav/NavGraph.kt
+++ app/src/main/java/com/android/cleanarchitecture/presentation/nav/NavGraph.kt
@@ -1,13 +1,36 @@
 package com.android.cleanarchitecture.presentation.nav
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.android.cleanarchitecture.presentation.ui.CityDetailScreen
+import com.android.cleanarchitecture.presentation.ui.CityListScreen
+import com.android.cleanarchitecture.presentation.viewmodel.CityViewModel
 
 @Composable
 fun NavGraph() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "list") {
-
+        composable("list") {
+            val vm: CityViewModel = hiltViewModel()
+            CityListScreen(navcontroller = navController, vm = vm)
+        }
+        composable(
+            route = "details/{cityId}",
+            arguments = listOf(navArgument("cityID") {
+                type = NavType.StringType
+            })
+        ) { backStackEntry ->
+            val viewmodel: CityViewModel = hiltViewModel()
+            CityDetailScreen(
+                navController = navController,
+                cityId = backStackEntry.arguments?.getString("cityId") ?: "",
+                viewmodel
+            )
+        }
     }
 }
